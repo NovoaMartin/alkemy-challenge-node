a807dfa39277f6bae15ff58e62ae09e00c4a5784
Martin Novoa <martinnovoar2@gmail.com>
2022-07-09 00:12:43 -0300

    Add database migration setup

---
+++ src/config/migrations/migration.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { Sequelize } from 'sequelize-typescript';
+import UserModel from '../../models/UserModel';
+import CharacterModel from '../../models/CharacterModel';
+import GenreModel from '../../models/GenreModel';
+import FilmModel from '../../models/FilmModel';
+import FilmCharacterModel from '../../models/FilmCharacterModel';
+
+config();
+
+(async () => {
+  const sequelize = new Sequelize(process.env.DB_URI!, {
+    dialect: 'postgres',
+    models: [UserModel, CharacterModel, FilmModel, GenreModel, FilmCharacterModel],
+  });
+  await sequelize.sync({ force: true });
+})();
