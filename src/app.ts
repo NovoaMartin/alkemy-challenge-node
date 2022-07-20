c5bf59a87c910d1714cafa18f743b9739f54da09
Martin Novoa <martinnovoar2@gmail.com>
2022-07-20 16:42:45 -0300

    Auth module initialization

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 import configureDI from './config/di';
 import swaggerDocument from '../swagger.json';
+import initAuthModule from './modules/auth/module';
 
 dotenv.config();
 
@@ -11,23 +11,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-const swaggerJsdocOptions: swaggerJSDoc.Options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Alkemy challenge nodejs',
-      version: '1.0.0',
-    },
-  },
-  apis: ['./src/modules/**/*Controller.ts'],
-};
-
-// const openApiSpec = swaggerJSDoc(swaggerJsdocOptions);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// const container = configureDI();
+const container = configureDI();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -35,4 +22,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+initAuthModule(container, app);
+
+// eslint-disable-next-line no-console
 app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`); });
