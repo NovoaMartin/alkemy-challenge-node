b7847ddfd5ecd6c6882994506964b17191cb3d7a
Martin Novoa <martinnovoar2@gmail.com>
2022-07-19 22:40:17 -0300

    Add signIn test

--- test/modules/auth/authService/AuthService.test.ts
+++ test/modules/auth/authService/AuthService.test.ts
@@ -4,10 +4,15 @@ import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import sinonChai from 'sinon-chai';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
 import AuthRepository from '../../../../src/modules/auth/authRepository/AuthRepository';
 import AuthService from '../../../../src/modules/auth/authService/AuthService';
 import UserNotFoundException from '../../../../src/modules/auth/exception/UserNotFoundException';
 import User from '../../../../src/modules/auth/authEntity/User';
+import IncorrectPasswordException from '../../../../src/modules/auth/exception/IncorrectPasswordException';
+
+config();
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
@@ -20,7 +25,7 @@ describe('AuthService tests', () => {
   beforeEach(async () => {
     sandbox = sinon.createSandbox();
     authRepository = sandbox.createStubInstance(AuthRepository);
-    authService = new AuthService(authRepository);
+    authService = new AuthService(authRepository, bcrypt);
   });
   afterEach(() => {
     sandbox.restore();
@@ -44,9 +49,9 @@ describe('AuthService tests', () => {
   });
 
   describe('save test', () => {
-    it('calls repository save method correctly', () => {
+    it('calls repository save method correctly', async () => {
       const userData: User = new User('id', 'username', 'password', 'asd', new Date(), new Date());
-      authService.save(userData);
+      await authService.save(userData);
       expect(authRepository.save).to.have.been.calledOnceWith(userData);
       expect(bcrypt.compareSync('password', userData.password)).to.be.true;
     });
@@ -55,6 +60,36 @@ describe('AuthService tests', () => {
       authRepository.save.resolves(userData);
       const result = await authService.save(userData);
       expect(result).to.deep.equal(userData);
+    });
+  });
+
+  describe('signIn test', () => {
+    it('throws exception if no user is found', async () => {
+      authRepository.save.callsFake(() => { throw new UserNotFoundException(); });
+      expect(authService.signIn('username', 'password')).to.be.rejectedWith(UserNotFoundException);
+    });
+    it('throws exception if password is incorrect', async () => {
+      const userData: User = new User('id', 'username', 'password', 'asd', new Date(), new Date());
+      authRepository.getByUsername.resolves(userData);
+      expect(authService.signIn('wrong_username', 'password')).to.be.rejectedWith(IncorrectPasswordException);
+    });
+    it('calls repository getByUsername method correctly', async () => {
+      try {
+        await authService.signIn('username', 'password');
+      } catch (e) {
+        // Should throw exception
+      }
+      expect(authRepository.getByUsername).to.have.been.calledOnceWith('username');
+    });
+    it('returns a valid token on success', async () => {
+      const userData: User = new User('id', 'username', 'password', 'asd', new Date(), new Date());
+      userData.password = bcrypt.hashSync('password', 10);
+      authRepository.getByUsername.resolves(userData);
+      const token = await authService.signIn('username', 'password');
+      expect(token).to.be.a('string');
+      const result = jwt.verify(token, process.env.JWT_SECRET!);
+      expect(result).to.have.property('id', 'id');
+      expect(result).to.have.property('exp');
     });
   });
 });
