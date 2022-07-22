72c4f3221f26e32b0626c89806ebd5db13fd18ef
Martin Novoa <martinnovoar2@gmail.com>
2022-07-22 17:51:33 -0300

    Add characterController getById test

---
+++ src/modules/character/controller/CharacterController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import CharacterService from '../service/CharacterService';
+
+export default class CharacterController {
+  constructor(private characterService: CharacterService) {}
+
+  async getById(req: Request, res: Response) {
+
+  }
+}
---
+++ test/modules/character/controller/CharacterController.test.ts
@@ -0,0 +1,56 @@
+import { describe } from 'mocha';
+import sinon, { SinonSandbox, SinonStubbedInstance } from 'sinon';
+import chai, { expect } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import sinonChai from 'sinon-chai';
+import { config } from 'dotenv';
+import { mockReq, mockRes } from 'sinon-express-mock';
+import CharacterService from '../../../../src/modules/character/service/CharacterService';
+import CharacterController from '../../../../src/modules/character/controller/CharacterController';
+import Character from '../../../../src/modules/character/entity/Character';
+import CharacterNotFoundException from '../../../../src/modules/character/exception/CharacterNotFoundException';
+
+config();
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+
+describe('AuthController tests', () => {
+  let characterController: CharacterController;
+  let characterService: SinonStubbedInstance<CharacterService>;
+  let sandbox: SinonSandbox;
+  beforeEach(async () => {
+    sandbox = sinon.createSandbox();
+    characterService = sandbox.createStubInstance(CharacterService);
+    characterController = new CharacterController(characterService);
+  });
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe('getById', () => {
+    it('calls service with correct parameters', async () => {
+      const req = mockReq({ params: { id: '1' } });
+      const res = mockRes();
+      characterService.getById.resolves(new Character('1', 'name', 'image', 'story'));
+      await characterController.getById(req, res);
+      expect(characterService.getById).to.have.been.calledWith(1);
+    });
+    it('responds with found character', async () => {
+      const req = mockReq({ params: { id: '1' } });
+      const res = mockRes();
+      characterService.getById.resolves(new Character('1', 'name', 'image', 'story'));
+      await characterController.getById(req, res);
+      expect(res.json).to.have.been.calledWith(new Character('1', 'name', 'image', 'story'));
+      expect(res.status).to.have.been.calledWith(200);
+    });
+    it('responds with error if no character is found', async () => {
+      const req = mockReq({ params: { id: '1' } });
+      const res = mockRes();
+      characterService.getById.callsFake(async () => { throw new CharacterNotFoundException(); });
+      await characterController.getById(req, res);
+      expect(res.json).to.have.been.calledWith({ error: 'Character not found' });
+      expect(res.status).to.have.been.calledWith(404);
+    });
+  });
+});
