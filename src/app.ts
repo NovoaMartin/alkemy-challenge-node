024ddbd71d478d80fedfb62921120f1566a12ac5
Martin Novoa <martinnovoar2@gmail.com>
2022-07-30 16:39:24 -0300

    Setup genre module

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
-import path from 'path';
 import configureDI from './config/di';
 import swaggerDocument from '../swagger.json';
 import initCharacterModule from './modules/character/module';
 import initAuthModule from './modules/auth/module';
 import initFilmModule from './modules/film/module';
+import initGenreModule from './modules/genre/module';
 
 dotenv.config();
 
@@ -20,16 +20,12 @@ app.use(express.urlencoded({ extended: true }));
 const container = configureDI();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.use('/images', express.static(path.join(__dirname, '../', 'images')));
-
 initAuthModule(container, app);
 initCharacterModule(app, container);
 initFilmModule(app, container);
+initGenreModule(app, container);
+
+app.get('/', (req: Request, res: Response) => res.redirect('/api-docs'));
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => { console.log(`Server is listening on port ${PORT}`); });
